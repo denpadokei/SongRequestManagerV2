e62d3553991392a08b3fc9b20d98d1fbeb77e90d
denpadokei <jsoldenpa@yahoo.co.jp>
2023-06-26 22:42:05 +0900

    fix 1.30.2

--- SongRequestManagerV2/UI/SongListUtils.cs
+++ SongRequestManagerV2/UI/SongListUtils.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading;
 using UnityEngine;
 using Zenject;
@@ -31,7 +32,8 @@ namespace SongRequestManagerV2
             // get the tab bar
             var segcontrol = this._selectLevelCategoryViewController.GetField<IconSegmentedControl, SelectLevelCategoryViewController>("_levelFilterCategoryIconSegmentedControl");
             segcontrol.SelectCellWithNumber(index);
-            this._selectLevelCategoryViewController.LevelFilterCategoryIconSegmentedControlDidSelectCell(segcontrol, index);
+            var method = typeof(SelectLevelCategoryViewController).GetMethod("SelectLevelCategoryViewController", (BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance));
+            method?.Invoke(this._selectLevelCategoryViewController, new object[] { segcontrol, index });
         }
 
         public IEnumerator ScrollToLevel(string levelID, Action callback, bool isWip = false)
@@ -40,7 +42,8 @@ namespace SongRequestManagerV2
                 // Make sure our custom songpack is selected
                 this.SelectCustomSongPack(1);
 
-                this._levelFilteringNavigationController.UpdateCustomSongs();
+                var method = typeof(LevelFilteringNavigationController).GetMethod("UpdateCustomSongs", (BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance));
+                method.Invoke(this._levelFilteringNavigationController, new object[0]);
 
                 yield return new WaitWhile(() => this._levelFilteringNavigationController.GetField<CancellationTokenSource, LevelFilteringNavigationController>("_cancellationTokenSource") != null);
                 var gridView = this._annotatedBeatmapLevelCollectionsViewController.GetField<AnnotatedBeatmapLevelCollectionsGridView, AnnotatedBeatmapLevelCollectionsViewController>("_annotatedBeatmapLevelCollectionsGridView");
@@ -48,7 +51,8 @@ namespace SongRequestManagerV2
                 var customSong = isWip
                     ? gridView.GetField<IReadOnlyList<IAnnotatedBeatmapLevelCollection>, AnnotatedBeatmapLevelCollectionsGridView>("_annotatedBeatmapLevelCollections").ElementAt(1)
                     : gridView.GetField<IReadOnlyList<IAnnotatedBeatmapLevelCollection>, AnnotatedBeatmapLevelCollectionsGridView>("_annotatedBeatmapLevelCollections").FirstOrDefault();
-                this._annotatedBeatmapLevelCollectionsViewController.HandleDidSelectAnnotatedBeatmapLevelCollection(customSong);
+                method = typeof(AnnotatedBeatmapLevelCollectionsViewController).GetMethod("HandleDidSelectAnnotatedBeatmapLevelCollection", (BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance));
+                method?.Invoke(this._annotatedBeatmapLevelCollectionsViewController, new object[] { customSong });
                 var song = isWip ? Loader.GetLevelById($"custom_level_{levelID.Split('_').Last().ToUpper()} WIP") : Loader.GetLevelByHash(levelID.Split('_').Last());
                 if (song == null) {
                     yield break;
@@ -68,7 +72,8 @@ namespace SongRequestManagerV2
             if (RequestBotConfig.Instance?.ClearNoFail == true) {
                 var gameplayModifiersPanelController = this._gameplaySetupViewController.GetField<GameplayModifiersPanelController, GameplaySetupViewController>("_gameplayModifiersPanelController");
                 gameplayModifiersPanelController.gameplayModifiers.SetField("_noFailOn0Energy", false);
-                this._gameplaySetupViewController.RefreshActivePanel();
+                var method = typeof(GameplayModifiersPanelController).GetMethod("RefreshActivePanel", (BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance));
+                method?.Invoke(gameplayModifiersPanelController, new object[0]);
             }
             callback?.Invoke();
         }
